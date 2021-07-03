ac68afdeff564ff11b29552d90ebef26c17317ae
Anuj Singh <anuj.singh@myoperator.co>
2021-07-03 23:22:01 +0530

    updating seetings and yml file

--- xeo_users/settings/settings.py
+++ xeo_users/settings/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    ## Custom App ##
+    'service.apps.ServiceConfig'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +78,12 @@ WSGI_APPLICATION = 'xeo_users.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'xeo_users',
+        'USER': 'anuj',
+        'PASSWORD': 'password',
+        'HOST': 'xeo_db',
+        'PORT': 5432,
     }
 }
 
