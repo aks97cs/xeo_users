99f2c655200986456fc148e0db127e7ef58d5e90
Anuj Singh <anuj.singh@myoperator.co>
2021-07-10 00:19:36 +0530

    service url update

--- service/urls.py
+++ service/urls.py
@@ -3,7 +3,7 @@ from service.views import User
 from service.views import Auth
 
 urlpatterns = [
-    path('user/auth', Auth.as_view(), name='user_auth'),
+    path('auth', Auth.as_view(), name='user_auth'),
     path('user', User.as_view(), name="user_api"),
     path('user/<int:id>', User.as_view(), name="user_api_args"),
 ]
