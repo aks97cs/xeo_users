7c28a327faa8f65c917cb4c3572f80aacb45d8f9
Anuj Singh <anuj.singh@myoperator.co>
2021-07-10 02:48:16 +0530

    configured custom auth model with info section

---
+++ service/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+import uuid
+
+
+class User(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+
+class UserInfo(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='info')
+    address = models.TextField()
