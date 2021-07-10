cf1948986a0b84827ff8c403d85fdf6a6675f1f8
Anuj Singh <anuj.singh@myoperator.co>
2021-07-11 02:22:22 +0530

    updating docker-file

--- service/views/auth_view.py
+++ service/views/auth_view.py
@@ -10,9 +10,10 @@ class Auth(ObtainAuthToken):
                                            context={'request': request})
         serializer.is_valid(raise_exception=True)
         user = serializer.validated_data['user']
+        # token value: (<Token: a018aec7c5476ef8382c7a2803c64f4bb9432453>, False)
         token = Token.objects.get_or_create(user=user)
         return Response({
-            'token': token.key,
+            'token': token[0].key,
             'user_id': user.pk,
             'email': user.email
         })
